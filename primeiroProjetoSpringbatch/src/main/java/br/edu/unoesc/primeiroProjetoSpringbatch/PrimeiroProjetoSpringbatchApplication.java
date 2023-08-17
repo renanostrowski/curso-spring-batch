1d24d36471a3e287202a5238a618fba64a4f3cd5
Renan Ostrowski <renancarlosmuraro@gmail.com>
2023-08-17 14:54:04 -0300

    Primeiro projeto usando chunck, reader, processor e writer

---
+++ primeiroProjetoSpringbatch/src/main/java/br/edu/unoesc/primeiroProjetoSpringbatch/PrimeiroProjetoSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.primeiroProjetoSpringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbatchApplication.class, args);
+	}
+
+}
---
+++ primeiroProjetoSpringbatch/src/test/java/br/edu/unoesc/primeiroProjetoSpringbatch/PrimeiroProjetoSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.primeiroProjetoSpringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
