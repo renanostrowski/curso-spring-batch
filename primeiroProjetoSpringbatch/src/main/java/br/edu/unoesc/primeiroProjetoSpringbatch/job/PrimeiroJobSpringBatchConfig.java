b27242c68c6e4adb678e21ad9e08322a52c42ad6
Renan Ostrowski <renancarlosmuraro@gmail.com>
2023-08-21 15:18:31 -0300

    Projeto para leitura de arquivo de largura fixa

--- primeiroProjetoSpringbatch/src/main/java/br/edu/unoesc/primeiroProjetoSpringbatch/job/PrimeiroJobSpringBatchConfig.java
+++ primeiroProjetoSpringbatch/src/main/java/br/edu/unoesc/primeiroProjetoSpringbatch/job/PrimeiroJobSpringBatchConfig.java
@@ -1,10 +1,12 @@
 package br.edu.unoesc.primeiroProjetoSpringbatch.job;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersIncrementer;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,7 @@ public class PrimeiroJobSpringBatchConfig {
 		return jobBuilderFactory
 				.get("imprimeOlaJob")
 				.start(imprimeOlaStep)
+				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 }
