03b2dd128738e96e85cbc63bc22b741628609d9a
Renan Ostrowski <renancarlosmuraro@gmail.com>
2023-09-13 14:39:54 -0300

    Escrita em multiplos arquivos

---
+++ Writer/DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentario/reader/DemonstrativoOrcamentarioRodape.java
@@ -0,0 +1,40 @@
+package com.springbatch.demonstrativoorcamentario.reader;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.NumberFormat;
+import java.util.List;
+
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeWrite;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.stereotype.Component;
+
+import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
+
+@Component
+public class DemonstrativoOrcamentarioRodape implements FlatFileFooterCallback{
+
+	private double totalGeral = 0.0;
+	
+	@Override
+	public void writeFooter(Writer writer) throws IOException {
+		writer.append("\n");
+		writer.append(String.format("\t\t\t\t\t\t\t  Total: %s\n", NumberFormat.getCurrencyInstance().format(totalGeral)));
+		writer.append(String.format("\t\t\t\t\t\t\t  Código de Autenticação: %s", "fkyew6868fewjfhjjewf"));
+	}
+	
+	@BeforeWrite
+	public void beforeWrite(List<GrupoLancamento> grupos) {
+		for (GrupoLancamento grupo : grupos) {
+			totalGeral += grupo.getTotal();
+		}
+	}
+	
+	@AfterChunk
+	public void afterChunck(ChunkContext context) {
+		totalGeral = 0.0;
+	}
+
+}
---
+++ Writer/DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -0,0 +1,103 @@
+package com.springbatch.demonstrativoorcamentario.writer;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.MultiResourceItemWriter;
+import org.springframework.batch.item.file.ResourceSuffixCreator;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.builder.MultiResourceItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
+import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
+import com.springbatch.demonstrativoorcamentario.reader.DemonstrativoOrcamentarioRodape;
+
+@Configuration
+public class DemonstrativoOrcamentarioWriterConfig {
+
+	@StepScope
+	@Bean
+	public MultiResourceItemWriter<GrupoLancamento> multiDemostrativoOrcamentarioWriter(
+			@Value("#{jobParameters['demonstrativosOrcamentarios']}") Resource demonstrativosOrcamentarios,
+			FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter){
+		return new MultiResourceItemWriterBuilder<GrupoLancamento>()
+				.name("multiDemostrativoOrcamentarioWriter")
+				.resource(demonstrativosOrcamentarios)
+				.delegate(demonstrativoOrcamentarioWriter)
+				.resourceSuffixCreator(suffixCreator())
+				.itemCountLimitPerResource(1)
+				.build();
+	}
+	
+	private ResourceSuffixCreator suffixCreator() {
+		return new ResourceSuffixCreator() {
+			
+			@Override
+			public String getSuffix(int index) {
+				return index + ".txt";
+			}
+		};
+	}
+
+	@Bean
+	@StepScope
+	public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
+			@Value("#{jobParameters['demonstrativoOrcamentario']}") Resource demonstrativoOrcamentario,
+			DemonstrativoOrcamentarioRodape demonstrativoOrcamentarioRodape) {
+		return new FlatFileItemWriterBuilder<GrupoLancamento>()
+				.name("demonstrativoOrcamentarioWriter")
+				.resource(demonstrativoOrcamentario)
+				.lineAggregator(lineAggregator())
+				.headerCallback(headerCallback())
+				.footerCallback(demonstrativoOrcamentarioRodape)
+				.build();
+	}
+
+private FlatFileHeaderCallback headerCallback() {
+		return new FlatFileHeaderCallback() {
+			
+			@Override
+			public void writeHeader(Writer writer) throws IOException {
+				writer.append(String.format("SISTEMA INTEGRADO: XPTO \t\t\t\t DATA: %s\n", new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
+				writer.append(String.format("MÓDULO: ORÇAMENTO \t\t\t\t\t\t HORA: %s\n", new SimpleDateFormat("HH:MM").format(new Date())));
+				writer.append(String.format("\t\t\tDEMONSTRATIVO ORCAMENTARIO\n"));
+				writer.append(String.format("----------------------------------------------------------------------------\n"));
+				writer.append(String.format("CODIGO NOME VALOR\n"));
+				writer.append(String.format("\t Data Descricao Valor\n"));
+				writer.append(String.format("----------------------------------------------------------------------------\n"));
+				
+			}
+		};
+	}
+
+private LineAggregator<GrupoLancamento> lineAggregator() {
+	return new LineAggregator<GrupoLancamento>() {
+		
+		@Override
+		public String aggregate(GrupoLancamento grupoLancamento) {
+			String formatGrupoOrcamentario = String.format("[%d] %s - %s\n", grupoLancamento.getCodigoNaturezaDespesa(),
+					grupoLancamento.getDescricaoNaturezaDespesa(),
+					NumberFormat.getCurrencyInstance().format(grupoLancamento.getTotal()));
+			
+			StringBuilder strBuilder = new StringBuilder();
+			for (Lancamento lancamento : grupoLancamento.getLancamentos()) {
+				strBuilder.append(String.format("\t [%s] %s - %s\n", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
+						NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
+			}
+			String formatLancamentos = strBuilder.toString();
+			return formatGrupoOrcamentario + formatLancamentos;
+		}
+	};
+}
+}
